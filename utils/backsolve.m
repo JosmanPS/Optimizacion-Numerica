3402e1db84580baa0e31f21a479b1a16921d5843
josmanps <jmps2812@gmail.com>
2015-12-05 13:22:13 -0600

    Agregué que devuelva los valores para calcular la inercia

--- utils/backsolve.m
+++ utils/backsolve.m
@@ -1,10 +1,14 @@
-function d = backsolve(A, b)
+function [d, neg, ran] = backsolve(A, b)
 
-    [L,D,P,S] = ldl(A);
+    [L, D, P, S, neg, ran] = ldl(A);
     
+    %
+    % Solve the problem
+    %
     d = P'*(S*b);
     d = L\d;
     d = D\d;
     d = L'\d;
     d = P'\d;
     d = S*d;
+    
